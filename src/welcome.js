a1b740c01ff4c5f0bc27dba111c314a9133b5142
priyankavijayan1999-blip <priyankavijayan1999@gmail.com>
2025-12-24 18:23:18 +0530

    first commit

---
+++ src/welcome.js
@@ -0,0 +1,19 @@
+/*
+var city="chennai";
+let name="priya";
+const pincode=638004;
+name="giri";
+pincode=638004;
+console.log(city);
+console.log(name);
+console.log(pincode);
+*/
+/*let fullname: string="priya";
+let age: number=22;
+let isMarried: boolean=false;
+let score:any=100;
+let value:unknown="playwrihght";
+console.log(fullname,age,isMarried,score,value);
+*/
+var age = 26;
+console.log("age");
