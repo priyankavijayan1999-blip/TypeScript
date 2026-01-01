f19d627195aa27a5b2c68e280f057124233b9d71
priyankavijayan1999-blip <priyankavijayan1999@gmail.com>
2026-01-02 01:30:34 +0530

    Uncomment and organize various control flow examples in condition.ts

--- src/controlFlow/condition.ts
+++ src/controlFlow/condition.ts
@@ -13,7 +13,7 @@ while (i <= 50) {
 }
 */
 
-let i = 1;
+/* let i = 1;
 let count = 0; // To count even numbers
 
 while (i <= 50) {
@@ -25,3 +25,40 @@ while (i <= 50) {
 }
 
 console.log("Total even numbers:", count);
+*/
+/*let num = 10; // you can change the number and test
+
+if (num > 0) {
+  console.log("The number is Positive");
+} else if (num < 0) {
+  console.log("The number is Negative");
+} else {
+  console.log("The number is Zero");
+}
+*/
+/*
+let marks: number =20; 
+if (marks >= 90) { 
+console.log("Grade A"); 
+} else if (marks >= 75) { 
+console.log("Grade B"); 
+} else if (marks >= 50) { 
+console.log("Grade C"); 
+} else { 
+console.log("Fail");
+}
+*/
+/*let age: number = 17; 
+ (age >= 18) ? "You can vote " : "You are too young "; 
+console.log(age);
+*/
+/*
+let age: number = 20; 
+let canVote = (age >= 18) ? "You can vote" : "You are too young"; 
+console.log(canVote);
+*/
+/*
+let number = 4; 
+let result = (number % 2 === 0) ? "Even Number" : "Odd Number"; 
+console.log(result);
+*/
